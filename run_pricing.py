beff879d4076d9e07455c7c16d05896a05817b53
PP <mumuxi15@icloud.com>
2025-01-17 16:18:30 -0500

    add scrapy need better methods

--- run_pricing.py
+++ run_pricing.py
@@ -5,6 +5,9 @@ import pandas as pd
 import plotly.express as px
 from plotly.subplots import make_subplots
 from datetime import datetime
+import requests
+import json
+from bs4 import BeautifulSoup
 from config import cabins
 
 class PriceAlgo:
@@ -60,9 +63,12 @@ class PriceAlgo:
 
 	def test(self, df):
 		# df = df.dropna(how='any',axis=0)
-		df['date'] = df['Date'].dt.strftime('%m-%d')
-		# tb = pd.pivot_table(df, index='date',columns='year',values='Rate')
-		# print (tb)
+		df['year'] = df['Date'].dt.isocalendar().year
+		df['date'] = 'w' + df['Date'].dt.isocalendar().week.astype(str)+'-d' + df['dow'].astype(str)
+		df.to_csv('dates.csv')
+		tb = pd.pivot_table(df, index='date',columns='year',values='Rate')
+		print (tb)
+		tb.to_csv('tmp.csv')
 		# tb.to_csv('tmp.csv')
 		# gp = gp.groupby(['year', 'month','day'])['Rate'].mean().reset_index()  # .unstack().T)
 		# print (gp)
@@ -73,34 +79,63 @@ class PriceAlgo:
 		# self.plot_weekly_heatmap(df)
 		# self.plot_monthly_rate(df)
 		self.test(df)
-
-def get_calendar_view(property):
-
-	
-	new_date = pd.date_range(start=f"{year}-01-01", end=f"{year}-12-31", freq='D')
-	new_date = pd.DataFrame({'Date': new_date})
-	new_date['week'] = new_date['Date'].dt.isocalendar().week
-	new_date['dow']=new_date['Date'].dt.dayofweek
-#	df = pd.merge(df, new_date, on=['week', 'dow']) after prices are done 
-	
-
-	print (df)
 		
-def bnb_data_analysis(property=None,loc='Nashville'):
-	directory = property['path'].replace('transactions/honey',f'pricing/insidebnb/{loc}/09-2022/listings.csv')
-	df = pd.read_csv(directory, usecols=['name', 'host_id',
-		'neighbourhood', 'latitude', 'longitude', 'room_type', 'price',
-		'minimum_nights', 'number_of_reviews', 'last_review',
-		'reviews_per_month', 'calculated_host_listings_count',
-		'availability_365', 'number_of_reviews_ltm'])
-	df = df.loc[df['number_of_reviews']>0]
-	print (df['room_type'].unique())
+def data_analysis(property=None,loc='Nashville'):
+	df = pd.read_csv('data/nashville_listing.csv', usecols= ['id', 'listing_url', 'scrape_id', 'last_scraped',
+       'description', 'neighborhood_overview', 'host_id', 'host_since', 'host_location',
+       'host_response_time', 'host_acceptance_rate',
+       'host_is_superhost', 'host_neighbourhood', 'host_listings_count', 'neighbourhood_cleansed', 'latitude',
+       'longitude', 'room_type', 'accommodates', 'bathrooms',
+       'bathrooms_text', 'bedrooms', 'beds', 'amenities', 'price',
+       'minimum_nights', 'maximum_nights', 'minimum_nights_avg_ntm',
+       'maximum_nights_avg_ntm', 'has_availability',
+       'availability_30', 'availability_60', 'availability_90',
+       'availability_365', 'number_of_reviews',
+       'number_of_reviews_ltm', 'number_of_reviews_l30d',
+       'last_review', 'review_scores_rating', 'instant_bookable', 'reviews_per_month'])
+
+	print ('length: ',len(df))
+	df = df.loc[(df['reviews_per_month']>(1/12)) & (df['review_scores_rating']>4)]  #at least 1 reviews in a year
+	df = df.loc[df['room_type']=='Entire home/apt']
+	df = df.loc[df['last_review']>'2023-01-01']
+	df.to_csv('filtered_df.csv')
+	print ('length: ',len(df))
+	# print (sorted(df['last_review'].unique()))
+
+def airbnb_scrape():
+	url = "https://www.airbnb.com/s/Gatlinburg--Tennessee--United-States/homes?refinement_paths%5B%5D=%2Fhomes&flexible_trip_lengths%5B%5D=one_week&monthly_start_date=2025-02-01&monthly_length=3&monthly_end_date=2025-05-01&price_filter_input_type=0&channel=EXPLORE&query=Gatlinburg%2C%20TN&place_id=ChIJiaUIy-pTWYgRqHm3fq7XsUo&location_bb=Qg8HaMKm2x9CDsoewqci9w%3D%3D&date_picker_type=calendar&checkin=2025-02-01&checkout=2025-02-04&adults=2&source=structured_search_input_header&search_type=autocomplete_click"
+
+	response = requests.get(url)
+	soup = BeautifulSoup(response.content, "html.parser")
+	script_tag = soup.find("script", attrs={"data-injector-instances": "true"})
+	if script_tag:
+		# Extract the content of the script tag
+		data = json.loads(script_tag.string)['root > core-guest-spa'][1]
+		for d in data:
+			if isinstance(d, dict):
+				clientdata = d['niobeMinimalClientData']
+				print (len(clientdata[1]))
+
+				print (type(clientdata[1]))
+
+# print (scripts)
+	# for i, script in enumerate(soup.find_all('script')):
+	#
+	# 	print (script)
+	# 	print (i, '---'*10)
+	# script_tag = soup.find("script", {"id": "data-deferred-state-0"})
+	# print (soup)
+	# if script_tag:
+	# 	script_content = script_tag.string
+	# 	print(script_content)
 
 
 
-		
+
+
 # bnb_data_analysis(property=hh)
 
 SK = PriceAlgo(cabin = cabins['sky'])
-SK.run()
-# holidays()
+# SK.run()
+# data_analysis()
+airbnb_scrape()
